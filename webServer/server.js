14465dd1ed43b30c203c28ca367d3ad4e626dec3
romain <romainmichel@ynov.com>
2022-03-15 09:36:47 +0100

    mmm

--- webServer/server.js
+++ webServer/server.js
@@ -4,10 +4,10 @@ const fs = require('fs').promises;
 var sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./db/OGchinook.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
-      console.error(err.message);
+        console.error(err.message);
     }
     console.log('Connected to the database.');
-  });
+});
 
 
 // let sql = `SELECT * FROM tracks
@@ -17,7 +17,7 @@ let db = new sqlite3.Database('./db/OGchinook.db', sqlite3.OPEN_READWRITE, (err)
 //   if (err) {
 //     throw err;
 //   }
-  
+
 //   rows.forEach((row) => {
 //     console.log(row.Name);
 //   });
@@ -32,37 +32,37 @@ const port = 8000;
 
 
 const requestListener = function (req, res) {
-    var url = "/templates/home.html";
+    var url;
     switch (req.url) {
         case "/":
-            url="/templates/home.html";
+            url = "/templates/home.html";
             break
         case "/profil":
-            url="/templates/profil.html";
+            url = "/templates/profil.html";
             break
         case "/notifications":
-            url="/templates/notifications.html";
+            url = "/templates/notifications.html";
             break
         case "/disconnect":
-            url="/templates/disconnect.html";
+            url = "/templates/disconnect.html";
             break
         // default :
         //     url="/404.html";
     }
 
     fs.readFile(__dirname + url)
-            .then(contents => {
-                res.setHeader("Content-Type", "text/html");
-                res.writeHead(200);
-                res.end(contents);
-            })
-            .catch(err => {
-                res.writeHead(500);
-                res.end(toString(err));
-                console.error(`Could not read a file: ${err}`);
-                process.exit(1);
-            });
-            
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(toString(err));
+            console.error(`Could not read a file: ${err}`);
+            process.exit(1);
+        });
+
 };
 
 
