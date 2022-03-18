616fc634e2ce49d208c90b630045237a7244a7ba
romain <romainmichel@ynov.com>
2022-03-18 10:27:32 +0100

    menfou

--- endPoint/endpoint.js
+++ endPoint/endpoint.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const cors = require("cors");
+app.use(cors());
+
 app.get('/', (req, res) => {
     res.send('This is our enpoint')
 })
 
+app.post('/', (req, res) => {
+
+})
+
 app.post('/test', function (req, res) {
-    console.log(req.body);
+    console.log('jjjkjfijdkjdjj', req);
 })
 
 app.listen(port, () => {
